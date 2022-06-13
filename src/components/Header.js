99480e9cc56aa8f06198784851e990ebe379c168
codeyStein <san.26.2010@icloud.com>
2022-06-13 13:28:17 -0600

    Frist commit

---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+import React from "react"
+
+export default function Header() {
+    return (
+        <header className="header">
+            <img 
+                src="https://scrimba.com/blobs/sha1:ee3d2e1537021c55cfbfc3e54d0a9dec883a2f77.png"
+                className="header--image"
+            />
+            <h2 className="header--title">Meme Generator</h2>
+            <h4 className="header--project">React Course - Project 3</h4>
+        </header>
+    )
+}
